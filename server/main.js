a5dae63a9faa66caa80db5d41798ce2713dcc436
jaeminjung <jaeminjung@hotmail.com>
2018-12-06 20:05:52 +0900

    final

--- server/main.js
+++ server/main.js
@@ -110,7 +110,7 @@ function errorHandler(err, req, res, next) {
     res.status(res.statusCode || 500);
     res.json({
         message: err.message,
-        stack: err.stack
+        // stack: err.stack
     });
 }
 
